35aae878d534e6d622e4639b6764e1f044da900e
chenhaiyang <1559843332@qq.com>
2019-07-04 10:11:41 +0800

    集成钉钉报警

--- src/com/jdk18/lambda/Test1.java
+++ src/com/jdk18/lambda/Test1.java
@@ -1,17 +1,10 @@
 package com.jdk18.lambda;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
-import java.util.function.BiConsumer;
-
 import org.junit.Test;
+
+import java.util.*;
+import java.util.Map.Entry;
+import java.util.function.BiConsumer;
 
 public class Test1 {
     @Test
---
+++ src/newcom/thirdparty/dingding/DingDingSendMsg.java
@@ -0,0 +1,50 @@
+package newcom.thirdparty.dingding;
+
+import com.dingtalk.api.DefaultDingTalkClient;
+import com.dingtalk.api.DingTalkClient;
+import com.dingtalk.api.request.OapiRobotSendRequest;
+import com.dingtalk.api.response.OapiRobotSendResponse;
+
+import java.util.Arrays;
+
+/**
+ * jar包地址http://open-dev.dingtalk.com/download/openSDK/java
+ */
+public class DingDingSendMsg {
+
+    public static final String TOKEN = "***";
+
+    public static void main(String[] args) throws Exception{
+        DingTalkClient client = new DefaultDingTalkClient("https://oapi.dingtalk.com/robot/send?access_token="+TOKEN);
+        OapiRobotSendRequest request = new OapiRobotSendRequest();
+        request.setMsgtype("text");
+        OapiRobotSendRequest.Text text = new OapiRobotSendRequest.Text();
+        text.setContent("测试文本消息");
+        request.setText(text);
+        OapiRobotSendRequest.At at = new OapiRobotSendRequest.At();
+        at.setAtMobiles(Arrays.asList("13121939122"));
+        request.setAt(at);
+
+        request.setMsgtype("link");
+        OapiRobotSendRequest.Link link = new OapiRobotSendRequest.Link();
+        link.setMessageUrl("https://www.dingtalk.com/");
+        link.setPicUrl("");
+        link.setTitle("时代的火车向前开");
+        link.setText("这个即将发布的新版本，创始人陈航（花名“无招”）称它为“红树林”。\n" +
+                "而在此之前，每当面临重大升级，产品经理们都会取一个应景的代号，这一次，为什么是“红树林");
+        request.setLink(link);
+        OapiRobotSendResponse response = client.execute(request);
+        System.out.println(response);
+
+        request.setMsgtype("markdown");
+        OapiRobotSendRequest.Markdown markdown = new OapiRobotSendRequest.Markdown();
+        markdown.setTitle("杭州天气");
+        markdown.setText("#### 杭州天气 @156xxxx8827\n" +
+                "> 9度，西北风1级，空气良89，相对温度73%\n\n" +
+                "> ![screenshot](https://www.cnblogs.com/images/cnblogs_com/chywx/995120/t_langrensha.png)\n"  +
+                "> ###### 10点20分发布 [天气](http://www.thinkpage.cn/) \n");
+        request.setMarkdown(markdown);
+        OapiRobotSendResponse response2 = client.execute(request);
+        System.out.println(response2);
+    }
+}
