bb907abd1bb84b121ca0e8ac6c2c818e57d30076
陈海洋 <1559843332@qq.com>
2020-05-30 10:18:12 +0800

    leetcode

---
+++ src/leetcode/editor/cn/[1]两数之和.java
@@ -0,0 +1,45 @@
+package leetcode.editor.cn;//给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+//
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。 
+//
+// 
+//
+// 示例: 
+//
+// 给定 nums = [2, 7, 11, 15], target = 9
+//
+//因为 nums[0] + nums[1] = 2 + 7 = 9
+//所以返回 [0, 1]
+// 
+// Related Topics 数组 哈希表
+
+
+import org.junit.Test;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution1 {
+
+    public static void main(String[] args) {
+        int[] arr = {2, 7, 11, 15};
+        int[] ints = twoSum(arr, 9);
+        for (int anInt : ints) {
+            System.out.println(anInt);
+        }
+    }
+
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] ints = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    ints[0] = i;
+                    ints[1] = j;
+                }
+            }
+        }
+        return ints;
+    }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ src/leetcode/editor/cn/[7]整数反转.java
@@ -0,0 +1,44 @@
+package leetcode.editor.cn;//给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+//
+// 示例 1: 
+//
+// 输入: 123
+//输出: 321
+// 
+//
+// 示例 2: 
+//
+// 输入: -123
+//输出: -321
+// 
+//
+// 示例 3: 
+//
+// 输入: 120
+//输出: 21
+// 
+//
+// 注意: 
+//
+// 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231, 231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。 
+// Related Topics 数学
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+    public int reverse(int x) {
+        long n = 0;
+        while (x != 0) {
+            n = n * 10 + x % 10;
+            x = x / 10;
+        }
+        return (int) n == n ? (int) n : 0;
+    }
+
+
+    public static void main(String[] args) {
+//        System.out.println(reverse(245));
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
