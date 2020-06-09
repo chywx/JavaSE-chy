dc1baee8d01d2aa47a1502cbdb233e5beec9121a
陈海洋 <1559843332@qq.com>
2020-06-09 18:19:22 +0800

    最长回文子串

---
+++ src/leetcode/editor/cn/[5]最长回文子串.java
@@ -0,0 +1,55 @@
+package leetcode.editor.cn;//给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+//
+// 示例 1： 
+//
+// 输入: "babad"
+//输出: "bab"
+//注意: "aba" 也是一个有效答案。
+// 
+//
+// 示例 2： 
+//
+// 输入: "cbbd"
+//输出: "bb"
+// 
+// Related Topics 字符串 动态规划
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+// 未解决
+class Solution5 {
+
+    public static void main(String[] args) {
+        Solution5 solution = new Solution5();
+        System.out.println(solution.longestPalindrome("abcdsarersdfssaeeasdsssddsssaet"));
+//        System.out.println(solution.longestPalindrome("aba"));
+//        System.out.println(solution.longestPalindrome("a"));
+    }
+
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
