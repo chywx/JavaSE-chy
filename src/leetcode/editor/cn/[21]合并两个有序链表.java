c50b9378eeb680d52bfb0d061a6e87daa1a3aa9f
陈海洋 <1559843332@qq.com>
2020-06-09 15:56:32 +0800

    合并两个有序链表

--- src/leetcode/editor/cn/[21]合并两个有序链表.java
+++ src/leetcode/editor/cn/[21]合并两个有序链表.java
@@ -21,12 +21,53 @@ package leetcode.editor.cn;//将两个升序链表合并为一个新的 升序 �
  * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+class Solution21 {
+
+    public static void main(String[] args) {
+        Solution21 solution = new Solution21();
+        ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(6)));
+        ListNode l2 = new ListNode(2, new ListNode(3, new ListNode(5)));
+        System.out.println(solution.mergeTwoLists(l1, l2));
+    }
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 
+        ListNode listNode = new ListNode(0);
 
-        return null;
+        while (true) {
+            if (l1 == null) {
+                ListNode lastNode = getLast(listNode);
+                lastNode.next = l2;
+                return listNode.next;
+            }
+            if (l2 == null) {
+                ListNode lastNode = getLast(listNode);
+                lastNode.next = l1;
+                return listNode.next;
+            }
+            int val1 = l1.val;
+            int val2 = l2.val;
+
+            if (val1 < val2) {
+                ListNode lastNode = getLast(listNode);
+                lastNode.next = new ListNode(val1);
+                l1 = l1.next;
+            } else {
+                ListNode lastNode = getLast(listNode);
+                lastNode.next = new ListNode(val2);
+                l2 = l2.next;
+            }
+        }
+
+    }
+
+    ListNode getLast(ListNode listNode) {
+        ListNode nextNode = listNode.next;
+        while (nextNode != null) {
+            listNode = nextNode;
+            nextNode = nextNode.next;
+        }
+        return listNode;
     }
 }
 
@@ -45,6 +86,14 @@ class ListNode {
     ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+            "val=" + val +
+            ", next=" + next +
+            '}';
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
