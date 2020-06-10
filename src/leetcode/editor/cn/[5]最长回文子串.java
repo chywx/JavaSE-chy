9e9fb3f341fba3540815fdf079901a114c646b4d
陈海洋 <1559843332@qq.com>
2020-06-10 14:21:24 +0800

    最长回文子串 BruteForce

--- src/leetcode/editor/cn/[5]最长回文子串.java
+++ src/leetcode/editor/cn/[5]最长回文子串.java
@@ -21,7 +21,7 @@ class Solution5 {
 
     public static void main(String[] args) {
         Solution5 solution = new Solution5();
-        System.out.println(solution.longestPalindrome("abcdsarersdfssaeeasdsssddsssaet"));
+        System.out.println(solution.longestPalindrome("abcdsarersdfssaeeasdsssddsssaetadfew"));
 //        System.out.println(solution.longestPalindrome("aba"));
 //        System.out.println(solution.longestPalindrome("a"));
     }
@@ -34,6 +34,7 @@ class Solution5 {
         for (int i = 0; i < s.length(); i++) {
             int len1 = expandAroundCenter(s, i, i);
             int len2 = expandAroundCenter(s, i, i + 1);
+            System.out.println("len:" + len1 + "---" + len2);
             int len = Math.max(len1, len2);
             if (len > end - start) {
                 start = i - (len - 1) / 2;
@@ -50,6 +51,53 @@ class Solution5 {
             R++;
         }
         return R - L - 1;
+    }
+}
+
+class BruteForce {
+
+    public static void main(String[] args) {
+        BruteForce bruteForce = new BruteForce();
+        System.out.println(bruteForce.longestPalindrome("abcdsarersdfssaeeasdsssddsssaetadfew"));
+    }
+
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+
+        int maxLen = 1;
+        int begin = 0;
+        // s.charAt(i) 每次都会检查数组下标越界，因此先转换成字符数组
+        char[] charArray = s.toCharArray();
+
+        // 枚举所有长度大于 1 的子串 charArray[i..j]
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (j - i + 1 > maxLen && validPalindromic(charArray, i, j)) {
+                    maxLen = j - i + 1;
+                    begin = i;
+                    System.out.println(begin + "---" + maxLen);
+                    System.out.println(s.substring(begin, begin + maxLen));
+                }
+            }
+        }
+        return s.substring(begin, begin + maxLen);
+    }
+
+    /**
+     * 验证子串 s[left..right] 是否为回文串
+     */
+    private boolean validPalindromic(char[] charArray, int left, int right) {
+        while (left < right) {
+            if (charArray[left] != charArray[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
