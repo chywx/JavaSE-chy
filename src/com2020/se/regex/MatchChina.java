bed8c67e1d16baba7bba901a73e417ca15201d76
陈海洋 <1559843332@qq.com>
2020-06-16 14:51:12 +0800

    正则

---
+++ src/com2020/se/regex/MatchChina.java
@@ -0,0 +1,26 @@
+package com2020.se.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 功能描述
+ *
+ * @author chy
+ * @date 2020/6/16 0016
+ */
+public class MatchChina {
+
+    public static void main(String[] args) {
+        String s = "<receiver_name>陈平<发信息>■</receiver_name><发信息>";
+        String m = "<[\u4e00-\u9fa5]+>";
+        Pattern pattern = Pattern.compile(m);
+
+        Matcher ma = pattern.matcher(s);
+
+        while (ma.find()) {
+            System.out.println("find " + ma.group());
+        }
+    }
+
+}
