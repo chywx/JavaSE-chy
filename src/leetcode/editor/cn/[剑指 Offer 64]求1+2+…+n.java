307fca84b085fe87b6c764c2f07eca92455ef8da
dahai <1559843332@qq.com>
2021-03-31 14:52:56 +0800

    leet code 剑指offer 64完成

---
+++ src/leetcode/editor/cn/[剑指 Offer 64]求1+2+…+n.java
@@ -0,0 +1,50 @@
+package leetcode.editor.cn;//求 1+2+...+n ，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+//
+// 
+//
+// 示例 1： 
+//
+// 输入: n = 3
+//输出: 6
+// 
+//
+// 示例 2： 
+//
+// 输入: n = 9
+//输出: 45
+// 
+//
+// 
+//
+// 限制： 
+//
+// 
+// 1 <= n <= 10000 
+// 
+// 👍 293 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.sumNums(5));
+    }
+
+
+    int res = 0;
+    public int sumNums(int n) {
+        boolean x = n > 1 && sumNums(n - 1) > 0;
+        System.out.println(x);
+        res += n;
+        return res;
+    }
+
+    public int sumNums2(int n) {
+        return n == 1 ? 1 : n + sumNums2(--n);
+    }
+
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
