c1713a3723f2d06d261e9dc52899de113c8abba1
dahai <chenhaiyang@luckybus.me>
2021-08-23 18:29:25 +0800

    自定义线程池 线程工厂，拒绝策略

--- src/cn/year2021/interview/heyihong/区间合并.java
+++ src/cn/year2021/interview/heyihong/区间合并.java
@@ -1,5 +1,7 @@
 package cn.year2021.interview.heyihong;
 
+import java.util.HashMap;
+
 /**
  * @author chy
  * @date 2021年08月03日 下午 16:00
@@ -11,14 +13,34 @@ public class 区间合并 {
 
     public static void main(String[] args) {
 
-        int arr[][] = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
-
-        for (int[] ints : arr) {
 
 
+        HashMap<String, String> stringStringHashMap = new HashMap<>();
+        stringStringHashMap.put("aa", "aa");
+        stringStringHashMap.put("bb", "bb");
+        stringStringHashMap.put("cc", "bb");
+        System.out.println(stringStringHashMap);
+
+        System.out.println(1 & 15);
+        System.out.println(2 & 15);
+        System.out.println(17 & 15);
+
+        System.out.println();
+        System.out.println(8 ^ 11);
+
+        System.out.println();
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i & 15);
 
         }
 
+        System.out.println();
+        for (int i = 0; i < 1000; i++) {
+            int h;
+            String key = i+"a";
+            h = (h = key.hashCode()) ^ (h >>> 16);
+            System.out.println(h + " - " + key.hashCode());
+        }
 
     }
 
