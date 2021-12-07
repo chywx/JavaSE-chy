24837ade76d5b5096de71dd6ab4be01aa3ecb240
dahai-luckybus <1559843332@qq.com>
2021-12-07 19:34:41 +0800

    CompletableFuture test

--- src/cn/year2021/thread/future/TestCompletableFuture.java
+++ src/cn/year2021/thread/future/TestCompletableFuture.java
@@ -4,7 +4,10 @@ package cn.year2021.thread.future;/**
  * Description：
  */
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -19,6 +22,35 @@ public class TestCompletableFuture {
 
         runAsync();
         supplyAsync();
+
+        System.out.println("===============");
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.submit(() -> {
+            System.out.println("executorService 是否为守护线程 :" + Thread.currentThread().isDaemon());
+            return null;
+        });
+        final CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> {
+            System.out.println("this is lambda supplyAsync");
+            System.out.println("supplyAsync 是否为守护线程 " + Thread.currentThread().isDaemon());
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("this lambda is executed by forkJoinPool");
+            return "result1";
+        });
+        final CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+            System.out.println("this is task with executor");
+            System.out.println("supplyAsync 使用executorService 时是否为守护线程 : " + Thread.currentThread().isDaemon());
+            return "result2";
+        }, executorService);
+//        System.out.println(completableFuture.get());// 取消调用的话，this lambda is executed by forkJoinPool不会输出
+        System.out.println(future.get());
+        executorService.shutdown();
+
+//        Thread.sleep(3000);
 
     }
 
