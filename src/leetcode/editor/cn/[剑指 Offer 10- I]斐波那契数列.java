9fcb10f490f97a21d62543265fdb4edab95d7925
chenhaiyang <1559843332@qq.com>
2022-06-30 18:15:11 +0800

    LeetCode 剑指offer10 pass

---
+++ src/leetcode/editor/cn/[剑指 Offer 10- I]斐波那契数列.java
@@ -0,0 +1,58 @@
+package leetcode.editor.cn;//写一个函数，输入 n ，求斐波那契（Fibonacci）数列的第 n 项（即 F(N)）。斐波那契数列的定义如下：
+//
+//
+//F(0) = 0,   F(1) = 1
+//F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
+//
+// 斐波那契数列由 0 和 1 开始，之后的斐波那契数就是由之前的两数相加而得出。
+//
+// 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：n = 2
+//输出：1
+//
+//
+// 示例 2：
+//
+//
+//输入：n = 5
+//输出：5
+//
+//
+//
+//
+// 提示：
+//
+//
+// 0 <= n <= 100
+//
+// Related Topics 记忆化搜索 数学 动态规划 👍 373 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class SolutionOffer10 {
+
+    public static void main(String[] args) {
+        System.out.println(new SolutionOffer10().fib(20));
+    }
+
+    public int fib(int n) {
+        final int MOD = 1000000007;
+        if (n < 2) {
+            return n;
+        }
+        int p = 0, q = 0, r = 1;
+        for (int i = 2; i <= n; ++i) {
+            p = q;
+            q = r;
+            r = (p + q) % MOD;
+        }
+        return r;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
